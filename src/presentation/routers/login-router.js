3cb1ca575cd5cb80429c7ced8ed2256b57ba74e3
Leandro Frederico <leandro_frederico@hotmail.com>
2021-08-03 23:19:24 -0300

    modify httpResponse.badrequest to receiv a error

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -1,5 +1,5 @@
 import { serverError, badRequest, unauthorizedError, ok } from '../helpers/http-response'
-
+import MissingParamError from '../helpers/missing-param-error'
 class LoginRouter {
   constructor (authUseCase) {
     this.authUseCase = authUseCase
@@ -9,10 +9,10 @@ class LoginRouter {
     try {
       const { email, password } = httpRequest.body
       if (!email) {
-        return badRequest('email')
+        return badRequest(new MissingParamError('email'))
       }
       if (!password) {
-        return badRequest('password')
+        return badRequest(new MissingParamError('password'))
       }
       const accessToken = await this.authUseCase.auth(email, password)
       if (!accessToken) return unauthorizedError()
