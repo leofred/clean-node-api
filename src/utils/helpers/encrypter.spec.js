1c19b3b15a6b02eb7730c5e6f2afcf53033f567a
Leandro Frederico <leandro_frederico@hotmail.com>
2021-08-05 16:05:39 -0300

    ensure Encrypter calls bcrypt with correct values

--- src/utils/helpers/encrypter.spec.js
+++ src/utils/helpers/encrypter.spec.js
@@ -9,14 +9,21 @@ class Encrypter {
 describe('Encrypter', () => {
   test('Should return true if bcrypt return true', async () => {
     const sut = new Encrypter()
-    const isValid = await sut.compare('any_value', 'hashed')
+    const isValid = await sut.compare('any_value', 'hashed_value')
     expect(isValid).toBe(true)
   })
 
   test('Should return false if bcrypt return false', async () => {
     const sut = new Encrypter()
     bcrypt.isValid = false
-    const isValid = await sut.compare('any_value', 'hashed')
+    const isValid = await sut.compare('any_value', 'hashed_value')
     expect(isValid).toBe(false)
+  })
+
+  test('Should call bcrypt with correct values', async () => {
+    const sut = new Encrypter()
+    await sut.compare('any_value', 'hashed_value')
+    expect(bcrypt.value).toBe('any_value')
+    expect(bcrypt.hash).toBe('hashed_value')
   })
 })
