07ac69daebf148551122e189eb8f8bbcece1900f
Leandro Frederico <leandro_frederico@hotmail.com>
2021-08-05 16:43:36 -0300

    move sut to a factory helper method

--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -5,16 +5,20 @@ class TokenGenerator {
   }
 }
 
+const makeSut = () => {
+  return new TokenGenerator()
+}
+
 describe('Token Generator', () => {
   test('Should return null if JWT returns null', () => {
-    const sut = new TokenGenerator()
+    const sut = makeSut()
     jwt.token = null
     const token = sut.generate('any_id')
     expect(token).toBeNull()
   })
 
   test('Should return a token if JWT returns token', () => {
-    const sut = new TokenGenerator()
+    const sut = makeSut()
     const token = sut.generate('any_id')
     expect(token).toBe(jwt.token)
   })
