9f2e07e3dd54049a6b973cc53500d961e570c89b
Leandro Frederico <leandro_frederico@hotmail.com>
2021-08-05 12:30:39 -0300

    simplfy code with factory helpers

--- src/domain/usecases/auth-usecase.js
+++ src/domain/usecases/auth-usecase.js
@@ -12,7 +12,7 @@ export default class AuthUseCase {
 
     const user = await this.loadUserByEmailRepository.load(email)
     if (!user) return null
-    await this.encrypter.compare(password, user.password)
-    return null
+    const isValid = await this.encrypter.compare(password, user.password)
+    if (!isValid) return null
   }
 }
