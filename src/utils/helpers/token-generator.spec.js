fbb45dac16da78089aaa64f769cb8884d46acf18
Leandro Frederico <leandro_frederico@hotmail.com>
2021-08-05 16:30:34 -0300

    ensure TokenGenerator returns a token if JWT returns token

--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -1,14 +1,21 @@
-
+import jwt from 'jsonwebtoken'
 class TokenGenerator {
   generate (id) {
-    return null
+    return jwt.sign(id, 'secret')
   }
 }
 
 describe('Token Generator', () => {
   test('Should return null if JWT returns null', () => {
     const sut = new TokenGenerator()
+    jwt.token = null
     const token = sut.generate('any_id')
     expect(token).toBeNull()
+  })
+
+  test('Should return a token if JWT returns token', () => {
+    const sut = new TokenGenerator()
+    const token = sut.generate('any_id')
+    expect(token).toBe(jwt.token)
   })
 })
