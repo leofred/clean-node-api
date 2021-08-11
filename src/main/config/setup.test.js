ab6b710c0271eded1c7487174aa5addb398c09c8
Leandro Frederico <leandro_frederico@hotmail.com>
2021-08-11 00:29:43 -0300

    enable CORS

--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -7,9 +7,18 @@ describe('App Setup', () => {
       res.send('')
     })
 
-    const res = await request(app)
-      .get('/test_x_power_by')
-
+    const res = await request(app).get('/test_x_power_by')
     expect(res.headers['x-powered-by']).toBeUndefined()
+  })
+
+  test('Should enable CORS', async () => {
+    app.get('/test_cors', (req, res) => {
+      res.send('')
+    })
+
+    const res = await request(app).get('/test_cors')
+    expect(res.headers['access-control-allow-origin']).toBe('*')
+    expect(res.headers['access-control-allow-methods']).toBe('*')
+    expect(res.headers['access-control-allow-headers']).toBe('*')
   })
 })
