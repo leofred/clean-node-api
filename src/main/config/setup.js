c84b0863904c8552677f258e1c5865b3064d16ae
Leandro Frederico <leandro_frederico@hotmail.com>
2021-08-11 00:48:41 -0300

    ensure requests return content-type as json by default

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,8 +1,10 @@
 const cors = require('../middlewares/cors')
+const contentType = require('../middlewares/content-type')
 const jsonParser = require('../middlewares/json-parser')
 
 module.exports = app => {
   app.disable('x-powered-by')
   app.use(cors)
   app.use(jsonParser)
+  app.use(contentType)
 }
