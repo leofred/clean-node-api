77cace33c76923942e44bf768257015755368679
Leandro Frederico <leandro_frederico@hotmail.com>
2021-08-11 00:23:45 -0300

    disable x-powered-by header

---
+++ src/main/config/setup.test.js
@@ -0,0 +1,15 @@
+const request = require('supertest')
+const app = require('./app')
+
+describe('App Setup', () => {
+  test('Should disable x-powered-by header', async () => {
+    app.get('/test_x_power_by', (req, res) => {
+      res.send('')
+    })
+
+    const res = await request(app)
+      .get('/test_x_power_by')
+
+    expect(res.headers['x-powered-by']).toBeUndefined()
+  })
+})
