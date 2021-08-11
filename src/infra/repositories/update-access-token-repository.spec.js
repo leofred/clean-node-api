131222c9a4f6084cd3a8098aaa15913f5e35b216
Leandro Frederico <leandro_frederico@hotmail.com>
2021-08-10 23:30:23 -0300

    ensure UpdateAccessTokenRepository throws if no userModel is provided

--- src/infra/repositories/update-access-token-repository.spec.js
+++ src/infra/repositories/update-access-token-repository.spec.js
@@ -33,7 +33,6 @@ describe('UpdateAccessToken Repository', () => {
 
   test('Should update the user with the given accessToken', async () => {
     const userModel = db.collection('users')
-
     const sut = new UpdateAccessTokenRepository(userModel)
 
     let fakeUser = {
@@ -48,5 +47,21 @@ describe('UpdateAccessToken Repository', () => {
     await sut.update(fakeUser._id, 'valid_token')
     const updatedFakeUser = await userModel.findOne({ _id: fakeUser._id })
     expect(updatedFakeUser.accessToken).toBe('valid_token')
+  })
+
+  test('Should throw if no UserModel is provided', async () => {
+    const sut = new UpdateAccessTokenRepository()
+    const userModel = db.collection('users')
+    let fakeUser = {
+      email: 'valid_email@email.com',
+      name: 'any_name',
+      age: 50,
+      password: 'hashed_password'
+    }
+    const { insertedId } = await userModel.insertOne(fakeUser)
+    fakeUser = { _id: insertedId, ...fakeUser }
+
+    const promisse = sut.update(fakeUser._id, 'valid_token')
+    await expect(promisse).rejects.toThrow()
   })
 })
